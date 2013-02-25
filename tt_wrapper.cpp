7db15e84eb2ff8cabae5dfa501f02bc9cefb3342
Bas Zoetekouw <bas@debian.org>
2013-02-25 07:57:12 +0100

    Started perl-C glue code

--- tt_wrapper.cpp
+++ tt_wrapper.cpp
@@ -2,12 +2,22 @@
 #include "teletekst.h"
 
 #include <cstddef>
+#include <cmalloc>
 
 void ParseGif( const char * filename, const char ** md5s, 
 		size_t * width, size_t * height )
 {
-	// bogus code
-	*md5s=filename;
-	*width = * height;
+	auto img = Image(filename);
+
+	auto width  = img.GetWidth();
+	auto height = img.GetHeight();
+
+	for (size_t y=0; y<height; y++)
+	{
+		for (size_t x=0; x<width; x++)
+		{
+
+		}
+	}
 }
 
